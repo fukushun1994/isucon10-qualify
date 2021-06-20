479b0fdea247dea5b150cbd40b5d769178843c3d
FUKUNAGA Shunya <fukushun1994@gmail.com>
2021-06-20 14:32:28 +0900

    add alp script

---
+++ bench/alp.sh
@@ -0,0 +1,1 @@
+alp ltsv -m '/api/recommended_estate/[0-9]+','/api/chair/[0-9]+','/api/estate/[0-9]+','/api/estate/req_doc/[0-9]+','/api/chair/buy/[0-9]+' --file /var/log/nginx/access.log --sort=avg
