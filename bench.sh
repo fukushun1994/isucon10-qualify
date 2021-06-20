bf060b915a9ed9ca40bdb5c768315463a2d8b320
FUKUNAGA Shunya <fukushun1994@gmail.com>
2021-06-20 16:07:42 +0900

    change bench run script

--- bench.sh
+++ bench.sh
@@ -1,5 +1,8 @@
+pushd bench && \
 sudo bash -c 'echo "" > /var/log/mysql/mysql-slow.log' && \
 sudo bash -c 'echo "" > /var/log/nginx/access.log' && \
 sudo systemctl restart mysql.service && \
 sudo systemctl reload nginx && \
-./bench -target-url http://127.0.0.1
+sudo systemctl restart isuumo.go.service && \
+./bench -target-url http://127.0.0.1 && \
+popd
