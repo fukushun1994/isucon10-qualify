ae87a3984f9b2134e3e010ce5a6a71eb4cccf307
FUKUNAGA Shunya <fukushun1994@gmail.com>
2021-06-20 18:19:00 +0900

    fix columns definition

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -40,7 +40,7 @@ CREATE TABLE isuumo.chair
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
     stock       INTEGER         NOT NULL,
-    rev_popularity AS (-popularity),
+    rev_popularity INTEGER AS (-popularity),
     INDEX price_id(price),
     INDEX rev_popularity_id(rev_popularity)
 );
