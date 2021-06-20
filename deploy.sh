c52b515390582fddcd62877bfdb62530966aefec
FUKUNAGA Shunya <fukushun1994@gmail.com>
2021-06-20 17:02:26 +0900

    copy mysqld.cnf manually

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 git pull && \
+sudo cp mysql/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf && \
 sudo bash -c 'echo "" > /var/log/mysql/mysql-slow.log' && \
 sudo bash -c 'echo "" > /var/log/nginx/access.log' && \
 sudo systemctl restart mysql.service && \
