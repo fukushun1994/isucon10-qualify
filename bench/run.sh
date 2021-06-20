de99617f3541148b010339825bb305430612c4ec
FUKUNAGA Shunya <fukushun1994@gmail.com>
2021-06-20 15:47:28 +0900

    add log deletion

--- bench/run.sh
+++ bench/run.sh
@@ -1,1 +1,5 @@
+sudo bash -c 'echo "" > /var/log/mysql/mysql-slow.log' && \
+sudo bash -c 'echo "" > /var/log/nginx/access.log' && \
+sudo systemctl restart mysql.service && \
+sudo systemctl reload nginx && \
 ./bench -target-url http://127.0.0.1
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 git pull && \
+sudo bash -c 'echo "" > /var/log/mysql/mysql-slow.log' && \
+sudo bash -c 'echo "" > /var/log/nginx/access.log' && \
 sudo systemctl restart mysql.service && \
 sudo systemctl reload nginx && \
 pushd webapp/go/ && \
